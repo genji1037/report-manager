9dfdcfc67c30e2c80291edc058383ceb5804fa62
yinyan.chen <386204174@qq.com>
2021-10-12 16:49:42 +0800

    fix

--- cmd/file_server/main.go
+++ cmd/file_server/main.go
@@ -15,8 +15,12 @@ var addr string
 func main() {
 	flag.StringVar(&addr, "addr", "", "")
 	e := gin.New()
-	e.GET(validPrefix+"/:path", func(c *gin.Context) {
-		path := validPrefix + c.Param("path")
+	e.GET("file", func(c *gin.Context) {
+		path := c.Query("path")
+		if !strings.HasPrefix(path, validPrefix) {
+			c.JSON(http.StatusForbidden, "invalid prefix")
+			return
+		}
 		i := strings.LastIndex(path, "/")
 		fPath, err := alg.GetFilePath(path[:i], path[i+1:])
 		if err != nil {
